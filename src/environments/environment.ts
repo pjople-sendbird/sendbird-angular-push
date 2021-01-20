0dbbda74a950c8c956cf996c4a305b289a4d8cbf
Walter Rodriguez (Sendbird) <walter.rodriguez@sendbird.com>
2021-01-20 11:26:36 +0000

    Clearing values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment = {
      * https://console.firebase.google.com/
      */
     firebase: {
-        apiKey: "AIzaSyDofYDkLu1QK8wDIy8-as4nqS5UB0gDHKY",
-        authDomain: "sendbird-push-9c7e5.firebaseapp.com",
-        databaseURL: "https://sendbird-push-9c7e5.firebaseio.com",
-        projectId: "sendbird-push-9c7e5",
-        storageBucket: "sendbird-push-9c7e5.appspot.com",
-        messagingSenderId: "202211528502",
-        appId: "1:202211528502:web:220558c0be3d04d29e2d97",
-        measurementId: "G-FHJ236Q88G"    
+        apiKey: "...",
+        authDomain: "...",
+        databaseURL: "...",
+        projectId: "...",
+        storageBucket: "...",
+        messagingSenderId: "...",
+        appId: "...",
+        measurementId: "..."   
     }
 };
 
